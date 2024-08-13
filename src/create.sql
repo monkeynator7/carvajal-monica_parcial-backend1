e94386f15905000a160d55560b19c8f42b4f88de
Monica <mcarvajalr@icloud.com>
2024-08-13 00:48:09 -0400

    update 1 - error test

--- src/create.sql
+++ src/create.sql
@@ -1,8 +1,8 @@
-DROP TABLE ODONTOLOGOS IF EXISTS;
+DROP TABLE IF EXISTS ODONTOLOGOS;
 
-CREATE TABLE ODONTOLOGOS(
-                            ID INT AUTO_INCREMENT PRIMARY KEY,
-                            NUMERO_MATRICULA VARCHAR(10) NOT NULL,
-                            NOMBRE VARCHAR(10) NOT NULL,
-                            APELLIDO VARCHAR(10) NOT NULL
+CREATE TABLE ODONTOLOGOS (
+                             ID INT IDENTITY PRIMARY KEY,
+                             NUMERO_MATRICULA VARCHAR(10) NOT NULL,
+                             NOMBRE VARCHAR(10) NOT NULL,
+                             APELLIDO VARCHAR(10) NOT NULL
 );
